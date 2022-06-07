00e1399da1daec6e68f162a6ed751ce79d5a89a5
Dragondes <cevs12321@gmail.com>
2022-06-06 23:09:11 -0400

    mejoras en las pruebas

--- features/step_definitions/editProfile.rb
+++ features/step_definitions/editProfile.rb
@@ -5,8 +5,6 @@ When('I change my data in the form to Edit my profile') do |table|
     data = table.rows_hash
     data.each_pair do |key, value|
       case key
-        when "Ocupacion"
-          fill_in 'Ocupacion', :with => value
         when "Profesion u oficio"
           fill_in 'carrera', :with => value	 
         when "Ciudad de residencia"
@@ -28,7 +26,29 @@ end
 When('I erase the career field') do
     fill_in 'carrera', :with => ""	 
 end
-Then('I should press the "Editar Perfil" button and see the change on career {string}') do |career|
+Then('I should press the "Editar Perfil" button') do 
   find(:xpath, "/html/body/div/div[2]/div[1]/div/div/div/div[2]").click
-  page.should have_field('carrera', with: career)
+  #page.should have_field('carrera', with: career)
+end
+
+Then('see the changes') do |table|
+  data = table.rows_hash
+  data.each_pair do |key, value|
+    case key
+      when "Profesion u oficio"
+        page.should have_field('carrera', with: value)
+      when "Ciudad de residencia"
+        page.should have_field('Ciudad de residencia', with: value)
+      when "Genero"
+        page.should have_field('genero', with: value)
+      when "Nombre de contacto de emergencia"
+          page.should have_field('nombre_contacto_de_emergencia', with: value)
+      when "Relación con contacto de emergencia"
+          page.should have_field('relacion_contacto_de_emergencia', with: value)
+      when "Número de contacto de emergencia"
+          page.should have_field('numero_contacto_de_emergencia', with: value) 
+      when "Mi pequeña descripción"	
+          page.should have_field('descripcion_personal', with: value)
+    end 
+  end
 end
